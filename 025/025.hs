47238b3139dd42721a8eb8d9e886220f79f17db2
Connor Williams <jricacosta+github@gmail.com>
2013-06-23 00:06:41 -0700

    tweak 025.hs

--- 025/025.hs
+++ 025/025.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE BangPatterns #-}
 
-main = print . length . takeWhile ((< 1000) . length) . fmap show $ fibs
+main = print . (+1) . length . takeWhile (< 10 ^ 999) $ fibs
 
 fibs = 1 : fib' 0 1
   where fib' !a b = let z = a + b in z : fib' b z
