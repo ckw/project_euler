5860d3e64125928def2fbdbd3f6b0a8388ec3737
Connor Williams <jricacosta+github@gmail.com>
2013-07-01 00:01:27 -0700

    tweak 027.hs

--- 027/027.hs
+++ 027/027.hs
@@ -2,14 +2,11 @@ import Data.Numbers.Primes (isPrime)
 import Data.Ord (comparing)
 import Data.List (maximumBy)
 
-main = print $ let ((a,b), len) = maximumBy (comparing snd) quadratics
-               in ((a,b), len, a * b)
+main = print $ let x@((a,b), _) = maximumBy (comparing snd) quadratics
+               in (x, a * b)
 
 quadratics = do a <- [-999, -998.. 999]
                 b <- [-999, -998.. 999]
-                return $ ((a, b), primeSequenceLength $ quadratic a b)
-
-quadratic a b = let f n = n * n + a * n + b
-                in fmap f [0..]
-
-primeSequenceLength = length . takeWhile isPrime
+                return $ ((a, b), length . takeWhile isPrime $ quadratic a b)
+  where quadratic a b = let f n = n * n + a * n + b
+                        in fmap f [0..]
